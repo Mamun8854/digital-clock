d549d3e3e14b26595dadbf9b00834f880f42a999
Mamun8854 <mamunabdullah8854@gmail.com>
2022-09-16 10:17:23 +0600

    clock complete

---
+++ app.js
@@ -0,0 +1,28 @@
+function showTime() {
+  let date = new Date();
+  let h = date.getHours(); // hours time 0 - 23
+  let m = date.getMinutes(); // minutes time 0 - 59
+  let s = date.getSeconds(); // seconds time 0 - 59
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+}
+
+showTime();
